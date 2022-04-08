28bfb4ecac66808517ce0e6dd81de2d77df0d8fb
Prasad <prasadj007official@gmail.com>
2022-04-08 17:17:18 +0530

    Added Login Page Using Servlet

---
+++ src/main/java/com/myfirstservlet/LoginServlet.java
@@ -0,0 +1,40 @@
+package com.myfirstservlet;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+description="Login Servlet Testing",
+urlPatterns = {"/LoginServlet"},
+initParams = {
+        @WebInitParam(name="user", value = "Prasad"),
+        @WebInitParam(name="password", value ="@prasad")
+        }
+)
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String user = req.getParameter("user");
+        String pwd = req.getParameter("pwd");
+        String userID = getServletConfig().getInitParameter("user");
+        String password = getServletConfig().getInitParameter("password");
+        if(userID.equals(user) && password.equals(pwd)) {
+            req.setAttribute("user", user);
+            req.getRequestDispatcher("LoginSuccess.jsp").forward(req, resp);
+        }
+            else{
+                RequestDispatcher rd = getServletContext().getRequestDispatcher("/Login.html");
+                PrintWriter out = resp.getWriter();
+                out.println("<font color=red>Either Username or password is Incorrect.</font>");
+                rd.include(req, resp);
+            }
+
+    }
+}
+
---
+++ target/ServletDemo/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>My First Servlet from index.jsp</h2>
+</body>
+</html>
